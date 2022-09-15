b7b828295ea0e60a3e9b5611edcacf69ec6078b4
Newton Carvalho <newtoncleber@gmail.com>
2022-09-15 19:09:44 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Livewire\Dashboard;
+use App\Http\Livewire\Home;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', Home::class)->name('home');
+Route::get('/dashboard', Dashboard::class)->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
